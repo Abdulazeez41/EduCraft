ccef1393e60d23f7ba9588c06c75031b39326cc6
Abdulazeez41 <abdulazezsalihu41@gmail.com>
2025-02-05 02:51:56 +0100

    feat(courses): add category and difficulty fields to course forms

--- app/courses/forms.py
+++ app/courses/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, IntegerField, DateField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, DateField, FileField
+from wtforms.validators import DataRequired, Optional
+from flask_wtf.file import FileAllowed
 
 class CourseForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     instructor_name = StringField('Instructor Name', validators=[DataRequired()])
     date_created = DateField('Start Date (YYYY-MM-DD)', format='%Y-%m-%d', validators=[DataRequired()])
-    prerequisites = TextAreaField('Prerequisites')
+    prerequisites = TextAreaField('Prerequisites', validators=[Optional()])
+    media = FileField('Upload Image/Video', validators=[Optional(), FileAllowed(['jpg', 'png', 'mp4', 'avi', 'pdf'], 'PDF, Images and videos only!')])
+    category = StringField('Category', validators=[Optional()])
+    difficulty_level = StringField('Difficulty Level', validators=[Optional()])
     submit = SubmitField('Create Course')
